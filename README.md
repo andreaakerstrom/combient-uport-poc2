7e939758dd319b00b11f262c1ec66456c73d75ec
Andres Junge <ajunge@yaykuy.cl>
2016-05-04 20:00:04 -0300

    first try

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # uport-connect-mapping
 Mapping Server for uPort Connect
+
+## Expected Usage
+
+- uPort-enabled app generates a random ID (ie: 1234-5678-9082)
+- uPort-enabled app show the user a QR with URL: https://mapping.uport.me/map/{randomId}
+- uPort-enabled app polls (GET) https://mapping.uport.me/map/{randomId} , waits for `address` field.
+
+- uPort Mobile app scans URL from QR code
+- uPort do a POST to the URL https://mapping.uport.me/map/{randomId} (scanned URL) with `{"address": "<address>"}` as body parameter
+
+
+- uPort-enabled app gets the {address} and delete the id from the mapping server with `DELETE https://mapping.uport.me/map/{randomId}`
+
+
+## API
+### GET Address by ID
+
+`GET /map/:id`
+
+Id is not in the in-memory database it will create a new record.
+
+### POST Address
+
+`POST /map/:id`
+
+Headers:
+```
+Content-Type: application/json
+```
+
+Params:
+```
+{
+  "address": "0xaddr12312"
+}
+
+```
+
+### DELETE Address
+`DELETE /map/:id`
--- server.js
+++ server.js
@@ -7,8 +7,9 @@ var express = require("express"),
 
 
 var db = new loki("db.json");
-var resourceCollection = db.addCollection("resouces");
-loadResources(resourceCollection)
+var addrCollection = db.addCollection("addresses",{
+    unique: ['id']
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -16,8 +17,47 @@ app.use(methodOverride());
 
 var router = express.Router();
 
-router.get('/resources', function(req, res) {
-   res.send(resourceCollection.data);
+router.get('/map/:id', function(req, res) {
+  var id = req.params.id;
+  console.log("GET /map/"+id);
+  var r=addrCollection.by("id",id);
+  if(r == undefined){
+    console.log("NEW id!");
+    r={
+      id: id
+    }
+    addrCollection.insert(r);
+  }
+  console.log(r);
+  res.send(r);
+});
+
+router.post('/map/:id', function(req, res) {
+  var id = req.params.id;
+  console.log("POST /map/"+id);
+  var r=addrCollection.by("id",id);
+  if(r == undefined){
+    res.status(404).send(id+" not found!");
+  }else{
+    console.log(r);
+    var address = req.body.address;
+    r.address=address;
+    console.log(r);
+    addrCollection.update(r);
+    res.send("Updated!");
+  }
+});
+
+router.delete('/map/:id', function(req, res) {
+  var id = req.params.id;
+  console.log("DELETE /map/"+id);
+  var r=addrCollection.by("id",id);
+  if(r == undefined){
+    res.status(404).send(id+" not found!");
+  }else{
+    addrCollection.remove(r);
+    res.send("Deleted!");
+  }
 });
 
 app.use(router);
