a5acc4f1ead896ee72dbd5f3ce186a42d05591ac
Andres Junge <ajunge@yaykuy.cl>
2016-05-11 00:38:56 -0300

    fix 4 heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var http = require('http');
 var finalhandler = require('finalhandler');
 var serveStatic = require('serve-static');
 
-var serve = serveStatic("./dist/");
+var serve = serveStatic(__dirname + "/dist");
 
 var server = http.createServer(function(req, res) {
   var done = finalhandler(req, res);
