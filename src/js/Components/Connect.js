72dea3d3def8d12e95ec9469cd69b0c474a1292a
Andres Junge <ajunge@yaykuy.cl>
2016-06-17 16:31:44 -0400

    connect with no mapping server

--- src/js/Components/Connect.js
+++ src/js/Components/Connect.js
@@ -3,48 +3,25 @@ import QRCode from 'qrcode.react';
 import {Link} from 'react-router';
 import web3 from 'web3'
 
-const mappingUrl = 'http://mapping.uport.me/addr/';
 const registryAddress = '0xa9be82e93628abaac5ab557a9b3b02f711c0151c'
 var pollingInterval;
 
 const Connect = React.createClass({
   getInitialState: function() {
     return {
-      randomStr: this.randomString(16, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
       address: null,
-      error: null,
       personaAttributes: null
     };
   },
-  randomString: function(length,chars){
-    var result = '';
-    for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
-    return result;
-  },
-  checkMappingServer: function() {
-    $.ajax({
-      url: mappingUrl + this.state.randomStr,
-      dataType: 'json',
-      cache: false,
-      success: function(data) {
-        if (typeof(data.address) !== 'undefined') {
-          this.setState({address: data.address});
-        }
-        if (typeof(data.error) !== 'undefined') {
-          this.setState({error: data.error});
-        }
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(mappingUrl + this.state.randomStr, status, err.toString());
-      }.bind(this)
-    });
-  },
-  componentDidMount: function() {
-    pollingInterval = setInterval(this.checkMappingServer, 1500);
-    setTimeout(function(){
-      clearInterval(pollingInterval);
-    }, 120000);
 
+  locationHashChanged: function() {
+    if(location.hash.startsWith("#access_token")){
+      var addr=location.hash.substring(13+1);
+      this.setState({address: addr})
+    }
+  },
+
+  componentDidMount: function() {
     window.uportRegistry.setIpfsProvider(
       {
         host: 'ipfs.infura.io',
@@ -57,6 +34,8 @@ const Connect = React.createClass({
         'https://consensysnet.infura.io:8545'
       )
     );
+
+    window.addEventListener("hashchange", this.locationHashChanged, false);
 
   },
   componentDidUpdate: function() {
@@ -80,14 +59,11 @@ const Connect = React.createClass({
         });
       }
 
-      
-      clearInterval(pollingInterval)
       $('#qr').hide();
       $('#address').text(this.state.address);
       $('#success').show();
     }
     if (this.state.error) {
-      clearInterval(pollingInterval)
       $('#qr').hide();
       $('#error').text(this.state.error);
       $('#errorDiv').show();
@@ -99,9 +75,9 @@ const Connect = React.createClass({
       <div className="container centered" style={{maxWidth:'480px'}}>
         <img className="main-logo" src="img/uPort-logo.svg" alt="uPort" title="uPort Logo" style={{maxWidth:'90px',margin: '20px auto 40px',display: 'block'}} />
         <div id="qr">
-          <a href={ethUrl}><QRCode value={mappingUrl + this.state.randomStr} size={256} /></a>
+          <a href={ethUrl}><QRCode value={ethUrl} size={256} /></a>
           <br /><br />
-          <p><strong>Value : </strong>{mappingUrl + this.state.randomStr}</p>
+          <p><strong>Value : </strong>{ethUrl}</p>
         </div>
         <div id="success" style={{display: 'none'}}>
           <h3>Success! You have connected your uPort identity.</h3>
