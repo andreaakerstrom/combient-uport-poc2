e88706fb20f29d7e9173b18f65e18c9dcd49a5f3
Andres Junge <ajunge@yaykuy.cl>
2016-07-04 13:53:44 -0400

    customized set status

--- src/js/Components/Sign.js
+++ src/js/Components/Sign.js
@@ -3,10 +3,6 @@ import QRCode from 'qrcode.react';
 import {Link} from 'react-router';
 import qs from 'qs';
 
-const statusContractAddr='0x60dd15dec1732d6c8a6125b21f77d039821e5b93'
-const contractFunction='updateStatus'
-const contractFunctionParameter='string%20%22I%27m%20happy%22'
-const uri='ethereum:'+statusContractAddr+'?label=uPort%20TestApp&function='+contractFunction+'('+contractFunctionParameter+')';
 
 const mappingUrl = 'http://chasqui.uport.me/tx/';
 var pollingInterval;
@@ -17,6 +13,8 @@ const Sign = React.createClass({
       randomStr: this.randomString(16, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
       tx: null,
       error: null,
+      statusText : null,
+      uri: "",
     };
   },
   randomString: function(length,chars){
@@ -24,6 +22,8 @@ const Sign = React.createClass({
     for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
     return result;
   },
+
+
   checkMappingServer: function() {
     $.ajax({
       url: mappingUrl + this.state.randomStr,
@@ -55,14 +55,46 @@ const Sign = React.createClass({
 
   componentDidMount: function() {
     window.addEventListener("hashchange", this.locationHashChanged, false);
-    pollingInterval = setInterval(this.checkMappingServer, 1500);
-    setTimeout(function(){
-      clearInterval(pollingInterval);
-    }, 120000);
+  },
+
+  setStatus: function(){
+    var status=this.refs.statusInput.value;
+    console.log("set status:"+status);
+    this.setState({statusText: status});
+
+    this.setUri(status);
+  },
+
+  setUri: function(status){
+    const statusContractAddr='0x60dd15dec1732d6c8a6125b21f77d039821e5b93'
+    const contractFunction='updateStatus'
+    var contractFunctionParameter=encodeURI('string '+status);
+
+    var uri='ethereum:'+statusContractAddr+'?label=uPort%20TestApp&function='+contractFunction+'('+contractFunctionParameter+')';
+
+    this.setState({uri: uri});
 
   },
 
+
   componentDidUpdate: function() {
+    console.log("componentDidUpdate:");
+    console.log(this.state);
+    if(this.state.uri==""){
+      $('#status').show();
+      $('#qr').hide();
+      $('#tx').hide();
+      $('#success').hide();
+    }else{
+      $('#status').hide();
+      $('#qr').show();
+
+      pollingInterval = setInterval(this.checkMappingServer, 1500);
+      setTimeout(function(){
+        clearInterval(pollingInterval);
+      }, 120000);
+    }
+
     if (this.state.tx) {
       clearInterval(pollingInterval)
       $('#qr').hide();
@@ -77,14 +109,28 @@ const Sign = React.createClass({
     }
   },
   render: function() {
-    var ethUrl=uri + "&callback_url=" + window.location.href;
-    var uriFull=uri+"&callback_url="+mappingUrl + this.state.randomStr;
+    var ethUrl=this.state.uri + "&callback_url=" + window.location.href;
+    var uriFull=this.state.uri+"&callback_url="+mappingUrl + this.state.randomStr;
 
     return (
       <div className="container centered" style={{maxWidth:'400px'}}>
         <img className="main-logo" src="img/uPort-logo.svg" alt="uPort" title="uPort Logo" style={{maxWidth:'90px',margin: '20px auto 40px',display: 'block'}} />
-        Set Status to: <strong>I´m Happy</strong>
-        <div id="qr">
+
+        <div id="status">
+          <span id="currentStatus"></span>
+          <form action="javascript:void(0);">
+              <ol className="fields">
+                <li>
+                  <label>New Status:</label>
+                  <input id="statusInput" ref="statusInput" type="text" placeholder="Status" defaultValue="I'm Happy" />
+                </li>
+                <li>
+                    <button className="btn" onClick={this.setStatus}>Set new status!</button>
+                </li>
+              </ol>
+          </form>
+        </div>
+        <div id="qr" style={{display: 'none'}}>
           <a href={ethUrl}><QRCode value={uriFull} size={256} /></a>
           <br /><br />
           <div>Please scan this code with your uPortApp.<br/>
