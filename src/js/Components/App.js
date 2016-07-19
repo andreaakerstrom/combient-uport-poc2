6e1033c180422a697c5e5df61bbcb0a61b3f5d23
Joel Torstensson <oed3933@gmail.com>
2016-07-19 15:10:29 +0200

    Now using uport default qrDisplay

--- src/js/Components/App.js
+++ src/js/Components/App.js
@@ -4,14 +4,6 @@ import '../../sass/uport.scss';
 import Web3 from 'web3';
 import Uport from 'uport';
 
-const qrstyle = {
-  position: 'fixed',
-  top: '50%',
-  left: '50%',
-  /* bring your own prefixes */
-  transform: 'translate(-50%, -50%)'
-}
-
 const App = React.createClass({
   getInitialState() {
     var self = this;
@@ -20,30 +12,16 @@ const App = React.createClass({
       closeQr: self.closeQr
     };
     var web3 = new Web3();
-    var uport = new Uport("TestApp", qrDisplay);
+    var uport = new Uport("TestApp");
     var uportProvider = uport.getUportProvider('https://consensysnet.infura.io:8545');
     web3.setProvider(uportProvider);
 
     return {web3: web3};
   },
-  openQr(data) {
-    var qrImg = (
-      <a href={data}>
-        <QRCode value={data} size={256} />
-      </a>
-    );
-    this.setState({qr: qrImg});
-  },
-  closeQr() {
-    this.setState({qr: ""});
-  },
   render() {
     var childrenWithWeb3 = React.cloneElement(this.props.children, { web3: this.state.web3 });
     return (
       <div className="connect">
-        <div style={qrstyle}>
-          {this.state.qr}
-        </div>
         {childrenWithWeb3}
       </div>
     )
