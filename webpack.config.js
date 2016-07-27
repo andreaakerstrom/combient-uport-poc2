ce6c2a8aece7b3b0cce42aac764bca24c450d889
gbugyis <greg@bugyis.com>
2016-07-27 14:56:50 +0300

    Minify bundle output w/ webpack

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,10 @@ module.exports = {
         ignore: ['*.txt']
       }
     ),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: false,
+      mangle: false
+    }),
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify('development')
