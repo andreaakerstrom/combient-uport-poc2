6fb84e6f45ae796a9447b057bdbe4865b3647f4d
andreaakerstrom <andrea.akerstrom@gmail.com>
2017-02-09 14:56:47 +0100

    basic layout

--- app/javascripts/Components/AuditTrail.js
+++ app/javascripts/Components/AuditTrail.js
@@ -3,93 +3,207 @@ import React from 'react'
 import { Link } from 'react-router'
 import { Registry } from 'uport-lib'
 import { web3 } from '../web3setup.js'
+var UportRegistry = require('uport-registry')
 
 export default class AuditTrail extends React.Component {
   constructor (props) {
     super(props)
+
+    let registry = new UportRegistry()
+
+    this.connect = this.connect.bind(this)
     this.getAuditTrail = this.getAuditTrail.bind(this)
+    this.addNewClaim = this.addNewClaim.bind(this)
+
     this.state = {
-      personnummer: null,
-      address: null
+      registry: registry,
+      connect: false,
+      connectPerNb:null,
+      connectAdress: null,
+      connectPersonaAttributes: null,
+      connectError: null,
+      getLog: false,
+      logPerNbr: null,
+      logAdress: null,
+      addClaim: false,
+      claimTx: null,
+      claimStatus: null,
+      claimError: null
     }
   }
 
   componentDidUpdate () {
+    let self = this
 
-  /*  var identityfactoryContract = web3.eth.contract([{"constant":false,"inputs":[{"name":"userKey","type":"address"},{"name":"adminKey","type":"address"}],"name":"CreateProxyWithController","outputs":[],"type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"creator","type":"address"},{"indexed":false,"name":"proxy","type":"address"},{"indexed":false,"name":"controller","type":"address"}],"name":"IdentityCreated","type":"event"}]);
-    var proxyContract = web3.eth.contract([{"constant":false,"inputs":[{"name":"_owner","type":"address"}],"name":"transfer","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"addr","type":"address"}],"name":"isOwner","outputs":[{"name":"","type":"bool"}],"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":false,"inputs":[{"name":"destination","type":"address"},{"name":"value","type":"uint256"},{"name":"data","type":"bytes"}],"name":"forward","outputs":[],"type":"function"}]);
-    var ownerwithadminContract = web3.eth.contract([{"constant":true,"inputs":[],"name":"userKey","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":false,"inputs":[{"name":"newUserKey","type":"address"}],"name":"updateUserKey","outputs":[],"type":"function"},{"constant":true,"inputs":[],"name":"adminKey","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":false,"inputs":[{"name":"destination","type":"address"},{"name":"value","type":"uint256"},{"name":"data","type":"bytes"}],"name":"sendTx","outputs":[],"type":"function"},{"constant":true,"inputs":[],"name":"proxy","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":false,"inputs":[{"name":"newAdminKey","type":"address"}],"name":"updateAdminKey","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"type":"function"},{"inputs":[{"name":"proxyAddress","type":"address"},{"name":"_userKey","type":"address"},{"name":"_adminKey","type":"address"}],"type":"constructor"}]);
+   if (this.state.connect) {
+     if (this.state.connectAdress && !this.state.connectPersonaAttributes) {
 
-    var identityFactory = identityfactoryContract.at('0xadb4966858672ef5ed70894030526544f9a5acdd')
-
-    var proxyAddress;
-    var controllerAddress;
-    var creatorAddress = '0xb3b50f852c35da10181bbda055bd16efd4cccae6';
-    var event = identityFactory.IdentityCreated({creator: creatorAddress}, function(error, result) {
-            proxyAddress = result.args.proxy;
-            controllerAddress = result.args.controller;
-        });
-
-    var proxy = proxyContract.at(proxyAddress);
-    var controller = ownerwithadminContract.at(controllerAddress);
-
-    console.log('identityFactory: ')
-    console.log(proxyAddress)
-    console.log(controllerAddress)
-*/
-
-
-
-   if (this.state.address) {
-        const personaRegistry = new Registry({ web3: web3.currentProvider})
-        personaRegistry.getPublicProfile(this.state.address).then(profile => {
-          console.log(profile)
-          $('#attributeName').text(profile.name)
+       const personaRegistry = new Registry({ web3: web3.currentProvider})
+       personaRegistry.getPublicProfile(this.state.connectAdress).then(profile => {
+         console.log(profile)
+         self.setState({ connectPersonaAttributes: profile })
+         $('#attributeName').text(profile.name)
+       }).catch(function(e) {
+          console.log(e)
+          self.setState({connectError: e})
         })
-        $('#attributeAddress').text(this.state.address)
-        $('#attributePerNbr').text(this.state.personnummer)
-        $('#success').show()
-        $('#errorDiv').hide()
-    } else {
-      $('#errorDiv').show()
-      $('#success').hide()
+
+       $('#connect').hide()
+       $('#claim').show()
+       $('#attributeConnectAdress').text(this.state.connectAdress)
+       $('#connectSuccess').show()
+       $('#connectErrorDiv').hide()
+     }
+     if (this.state.connectError) {
+       $('#connect').hide()
+       $('#claim').hide()
+       $('#connectSuccess').hide()
+       $('#connectError').text(this.state.connectError)
+       $('#connectErrorDiv').show()
+     }
+
+
+   } else if (this.state.addClaim) {
+     if (this.state.claimTx) {
+       $('#claimTx').text(this.state.claimTx)
+       $('#ClaimSuccess').show()
+     }
+     if (this.state.claimError) {
+       $('#claimError').text(this.state.claimError)
+       $('#claimErrorDiv').show()
+     }
+   } else if (this.state.getLog) {
+      if (this.state.logAdress) {
+           const personaRegistry = new Registry({ web3: web3.currentProvider})
+           personaRegistry.getPublicProfile(this.state.logAdress).then(profile => {
+             console.log(profile)
+             $('#attributeLogName').text(profile.name)
+           })
+           $('#attributeLogAdress').text(this.state.logAdress)
+           $('#attributeLogPerNbr').text(this.state.logPerNbr)
+           $('#logSuccess').show()
+           $('#logErrorDiv').hide()
+       } else {
+         $('#logErrorDiv').show()
+         $('#logSuccess').hide()
+       }
     }
   }
 
-  getAuditTrail () {
-    let nbr = this.refs.perNbrInput.value
-    this.setState({personnummer: nbr})
+  connect () {
+    let self = this
+    web3.eth.getCoinbase(function (error, address) {
+      if (error) {
+        self.setState({connectError: error})
+      } else {
+      web3.eth.defaultAccount = address
+      self.setState({connectAdress: address})
+    }})
+    self.setState({getLog: false})
+    self.setState({connect: true})
+  }
 
-    if (nbr == '8801182067') {
-      this.setState({address: '0xb3b50f852c35da10181bbda055bd16efd4cccae6'})
+  addNewClaim () {
+    let self = this
+    let claimType = this.refs.claimTypeInput.value
+    let claimValue = this.refs.claimValueInput.value
+
+  /*  var attributes = {
+      "@context": "http://schema.org",
+      "@type": claimType,
+      "value": claimValue
+    }
+*/
+   this.setState({claimStatus: 'Mining in progress.'})
+    var attributes = {
+      "@context": "http://schema.org",
+      "@type": "Person",
+      "name": "Dea"
+    }
+
+    this.state.registry.setAttributes(attributes, function (err, txHash) {
+      if (err) {
+        self.setState({claimError: err})
+        this.setState({claimStatus: null})
+      }
+      console.log('Attributes set.')
+
+      self.setState({claimTx: txHash})
+      self.waitForMined(txHash, {blockNumber: null})
+    })
+  }
+
+  waitForMined (txHash, res) {
+    let self = this
+    if (res.blockNumber) {
+      /*self.state.status.getStatus.call(web3.eth.defaultAccount, function (e, r) {
+      })*/
+      this.setState({claimStatus: null})
     } else {
-      this.setState({address: null})
+      console.log('not mined yet.')
+      // check again in one sec.
+      setTimeout(function () {
+        web3.eth.getTransaction(txHash, function (e, r) {
+          self.waitForMined(txHash, r)
+        })
+      }, 1000)
     }
   }
 
+    getAuditTrail () {
+      let self = this
+      let nbr = this.refs.logPerNbrInput.value
+      self.setState({logPerNbr: nbr})
+
+      if (nbr == '8801182067') {
+        self.setState({logAdress: '0xb3b50f852c35da10181bbda055bd16efd4cccae6'})
+      } else {
+        self.setState({logAdress: null})
+      }
+      self.setState({getLog: true})
+      self.setState({connect: false})
+    }
 
   render () {
 
-    let attributesTable = (
+    let connectAttributesTable = (
+      <table>
+        <tbody>
+          <tr>
+            <td style={{textAlign: 'left'}}><strong>uPort Id:</strong></td>
+            <td><span id='attributeConnectAdress' /></td>
+          </tr>
+          <tr>
+            <td style={{textAlign: 'left'}}><strong>Name:</strong></td>
+            <td><span id='attributeConnectName' /></td>
+          </tr>
+        </tbody>
+      </table>
+    )
+
+    let logAttributesTable = (
       <table>
         <tbody>
         <tr>
           <td><strong>Name: </strong></td>
-          <td><span id='attributeName' /></td>
+          <td><span id='attributeLogName' /></td>
         </tr>
         <tr>
           <td><strong>Personal number: </strong></td>
-          <td><span id='attributePerNbr' /></td>
+          <td><span id='attributeLogPerNbr' /></td>
         </tr>
         <tr>
           <td><strong>Uport Id: </strong></td>
-          <td><span id='attributeAddress' /></td>
+          <td><span id='attributeLogAdress' /></td>
         </tr>
         </tbody>
       </table>
     )
 
     return (
+
+
+
       <div>
         <nav className="navbar navbar-default">
             <img className='main-logo' src='images/combient-logo.png' alt='uPort'
@@ -106,27 +220,80 @@ export default class AuditTrail extends React.Component {
         <br/>
         <br/>
 
-        <form action='javascript:void(0);'>
-          <label style={{margin: '20px 10px 20px 30px'}}>
-            <font size="3">Personal number</font>
-            <br/>
-            <input type="text" name="name" ref='perNbrInput' defaultValue="8801182067" style={{margin: '10px auto auto 30px'}} />
-          </label>
-          <button className='btn' onClick={this.getAuditTrail}>Search</button>
-        </form>
+        <section>
+            <section style={{width: '50%', float: 'left'}}>
+            <div className='container centered' style={{maxWidth: '480px',margin: '20px 10px 40px 50px'}}>
 
-        <div id='success' style={{display: 'none'}}>
-          <table className='persona' style={{margin: '20px 10px 20px 30px'}}>
-            <tbody>
-              <tr>
-                <td>{attributesTable}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div id='errorDiv' style={{display: 'none'}}>
-          <h4 style={{margin: '20px 10px 20px 30px'}}>Error! No uPort identity was found.</h4>
-        </div>
+              <div id='connect'>
+               <h3>Add claim</h3>
+                <button className='btn bigger' onClick={this.connect} type='submit'>Connect uPort</button>
+              </div>
+              <div id='connectSuccess' style={{display: 'none'}}>
+                <h3>Success! You have connected your uPort identity.</h3>
+                <table className='persona'>
+                  <tbody>
+                    <tr>
+                      <td>{connectAttributesTable}</td>
+                    </tr>
+                  </tbody>
+                </table>
+                <br/>
+              </div>
+              <div id='connectErrorDiv' style={{display: 'none'}}>
+                <h3>Error! You have NOT connected your uPort identity.</h3>
+                <p><strong></strong><span id='connectError' style={{display: 'inline-block'}} /> </p>
+              </div>
+
+
+              <div id='claim' style={{display: 'none'}}>
+                <h3>Add claim</h3>
+                      <label>Claim type: </label>
+                      <input type='text' ref='claimTypeInput' defaultValue='Personnummer' />
+                      <br/>
+                      <br/>
+                      <label>Claim value: </label>
+                      <input type='text' ref='claimValueInput' defaultValue='8801182067' />
+                    <br/>
+                    <br/>
+                      <button className='btn' onClick={this.addNewClaim}>Add</button>
+              </div>
+              <div id='claimMining'>
+                <h3 id='claimStatus'>{this.state.claimStatus}</h3>
+              </div>
+              <div id='claimSuccess' style={{display: 'none'}}>
+                <h3>Success! You have added a claim with Tx: </h3>
+                <p><strong></strong><span id='claimTx' style={{display: 'inline-block'}} /></p>
+              </div>
+              <div id='claimErrorDiv' style={{display: 'none'}}>
+                <h3>Error! You have NOT added a claim.</h3>
+                <p><strong></strong><span id='claimError' style={{display: 'inline-block'}} /></p>
+              </div>
+            </div>
+        </section>
+
+        <section style={{width: '50%', float: 'right'}}>
+              <label>
+               <h3>Personal number</h3>
+                <input type="text" ref='logPerNbrInput' defaultValue="8801182067"/>
+              </label>
+              <button className='btn' onClick={this.getAuditTrail}>Search</button>
+
+            <div id='logSuccess' style={{display: 'none'}}>
+            <br/>
+              <table className='persona'>
+                <tbody>
+                  <tr>
+                    <td>{logAttributesTable}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div id='logErrorDiv' style={{display: 'none'}}>
+              <h4>No uPort identity was found.</h4>
+            </div>
+            </section>
+        </section>
+
       </div>
     )
   }
